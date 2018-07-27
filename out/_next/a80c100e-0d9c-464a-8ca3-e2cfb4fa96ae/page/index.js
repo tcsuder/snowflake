bac58bdd5dd0c137e179d8f43d20091d96f69eae
tcsuder@gmail.com <tcsuder@gmail.com>
2018-07-27 10:13:38 -0700

    Initial out subtree commit gh-pages

---
+++ out/_next/a80c100e-0d9c-464a-8ca3-e2cfb4fa96ae/page/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([6],{195:function(e,u,t){e.exports=t(196)},196:function(e,u,t){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(u,"__esModule",{value:!0});var r=t(0),a=n(r),l=t(103),o=n(l);u.default=function(e){var u=e.url.query,t=u.name,n=u.answerValues;return a.default.createElement(o.default,{name:t,answerValues:n})}}},[195]);
+            return { page: comp.default }
+          })
+        
